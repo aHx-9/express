3506ea8342b0b582b4eef7b76927d30d1fa907a3
aHx-9 <2201100023lo@cyber-u.ac.jp>
2026-07-30 03:32:11 +0900

    Lesson15 完成

--- routes/notes.js
+++ routes/notes.js
@@ -1,29 +1,24 @@
 var express = require('express');
 var router = express.Router();
-// レスポンスのデータ（ノート全件）
-const responseObjectDataAll = {
-textObject1 : {
-id: 1,
-title: 'ノート１のタイトルです',
-subTitle: 'ノート１のサブタイトルです',
-bodyText: 'ノート１の本文です'
-},
-textObject2 : {
-id: 2,
-title: 'ノート２のタイトルです',
-subTitle: 'ノート２のサブタイトルです',
-bodyText: 'ノート２の本文です'
-},
-};
-/**
-* メモを全件取得するAPI
-* @returns {Object[]} data
-* @returns {number} data.id - ID
-* @returns {string} data.title - タイトル
-* @returns {string} data.text - 内容
-*/
-router.get('/', function (req, res, next) {
-// 全件取得して返す
-res.json(responseObjectDataAll);
-})
+const { MongoClient } = require("mongodb");
+
+// 自分のURIに変更
+const uri = "mongodb+srv://hiroki49211_db_user:2124th@cluster0.7mqjvpd.mongodb.net/notes?appName=Cluster0";
+const client = new MongoClient(uri);
+
+router.get('/', async function(req, res, next) {
+
+    await client.connect();
+
+    const database = client.db("notes");
+    const notes = database.collection("notes");
+
+    const data = await notes.find({}).toArray();
+
+    res.json(data);
+
+    await client.close();
+
+});
+
 module.exports = router;
